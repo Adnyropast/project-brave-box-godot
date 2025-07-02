b99dfb02d7c490e9b6b17b320dd1f81589fe4ce3
Adnyropast <dealamu134579@gmail.com>
2025-07-02 16:53:34 +0200

    feat: add dynamic battle camera setting

--- globals/PlayerSettings.gd
+++ globals/PlayerSettings.gd
@@ -1,6 +1,10 @@
 extends Node
 
+var dynamic_battle_camera: bool = true
 var debug_mode: bool = false
+
+func toggle_dynamic_battle_camera() -> void:
+	dynamic_battle_camera = not dynamic_battle_camera
 
 func toggle_debug() -> void:
 	debug_mode = not debug_mode
--- scenes/menus_settings/menu_settings.gd
+++ scenes/menus_settings/menu_settings.gd
@@ -3,15 +3,29 @@ extends Control
 var menu_pause: Control
 
 func _ready() -> void:
-	update_button_debug()
+	update_all()
 
 func _on_button_close_pressed() -> void:
 	Menus.close_menu(self)
 	Menus.open_menu(menu_pause)
 
+func _on_button_dynamic_battle_camera_pressed() -> void:
+	PlayerSettings.toggle_dynamic_battle_camera()
+	update_button_dynamic_battle_camera()
+
 func _on_button_debug_pressed() -> void:
 	PlayerSettings.toggle_debug()
 	update_button_debug()
+
+func update_all():
+	update_button_debug()
+	update_button_dynamic_battle_camera()
+
+func update_button_dynamic_battle_camera() -> void:
+	if PlayerSettings.dynamic_battle_camera:
+		$MarginContainer/VBoxContainer/ButtonDynamicBattleCamera.text = "Dynamic Battle Camera (" + "On" + ")"
+	else:
+		$MarginContainer/VBoxContainer/ButtonDynamicBattleCamera.text = "Dynamic Battle Camera (" + "Off" + ")"
 
 func update_button_debug() -> void:
 	if PlayerSettings.debug_mode:
